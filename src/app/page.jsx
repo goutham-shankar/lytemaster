94d92d71612752d4d1313458717135d78c8c7386
Athul Anoop <athulanoop2020@gmail.com>
2025-01-12 20:35:25 +0530

    update: hero section UI completed with placeholder image in-place of video background

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,31 @@
 import Image from "next/image";
-
-export default function Home() {
+import Link from "next/link";
+const CtaButton = ({ name, href }) => {
   return (
-    <div>
-      <h1>LyteMaster</h1>
-    </div>
+    <button>
+      <Link href={href} className="px-8 py-2 border rounded-full">
+        {name}
+      </Link>
+    </button>
+  );
+};
+
+// TODO: Update hero placeholder image with a background video
+const Hero = () => {
+  return (
+    <section className="flex-1 flex flex-col justify-center items-center gap-4 bg-[url('../assets/landing/hero-placeholder.jpeg')] bg-cover bg-center text-white">
+      <h1 className="w-2/3 text-5xl font-bebasNeue uppercase text-center">
+        World class range of lighting solutions
+      </h1>
+      <CtaButton name="View More" href="#" />
+    </section>
+  );
+};
+
+export default function Landing() {
+  return (
+    <>
+      <Hero />
+    </>
   );
 }
