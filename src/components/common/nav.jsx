4478768546cf92b35292b85a9949a40b80a35df8
Athul Anoop <athulanoop2020@gmail.com>
2025-01-12 18:51:33 +0530

    add: common components - nav and footer (boilerplate)

---
+++ src/components/common/nav.jsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { ChevronDown } from "lucide-react";
+
+const navItems = [
+  { name: "Products", href: "#", dropdown: true },
+  { name: "Projects", href: "/projects", dropdown: false },
+  { name: "About us", href: "/about-us", dropdown: false },
+  { name: "News", href: "/news", dropdown: false },
+  { name: "Contact", href: "/contact", dropdown: false },
+];
+
+export default function Nav() {
+  return (
+    <nav className="w-full h-20 flex justify-evenly items-center bg-white">
+      <h1 className="text-center text-3xl font-dmSerifDisplay">LyteMaster</h1>
+      <ul className="flex justify-center items-center gap-3 text-sm">
+        {navItems.map((item, index) => (
+          <li key={index} className="mx-2">
+            <Link href={item.href} className="flex items-center gap-2">
+              {item.name}
+              {item.dropdown && <ChevronDown size={16} />}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
