2667bd307247f03d72e7894c7605b4e718111c7d
Athul Anoop <athulanoop2020@gmail.com>
2025-01-18 13:50:52 +0530

    update: improved text legibility

--- src/components/common/branding.jsx
+++ src/components/common/branding.jsx
@@ -3,7 +3,7 @@ import Logo from "@/components/common/logo";
 
 export default function Branding() {
   return (
-    <Link href={"/"} className="w-36 sm:w-48">
+    <Link href={"/"} className="w-36 sm:w-48 xl:w-56 2xl:w-72">
       <Logo />
     </Link>
   );
