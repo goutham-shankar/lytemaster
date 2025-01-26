48e113d9618e01cfb1b90074f91e641293016122
Athul Anoop <athulanoop2020@gmail.com>
2025-01-26 11:56:45 +0530

    add: components for text

--- src/components/common/text/heading.jsx
+++ src/components/common/text/heading.jsx
@@ -1,8 +1,7 @@
-export default function Title({ title, className = "" }) {
+export default function Heading({ title, className = "" }) {
   return (
     <h1 className={`${className} text-xl sm:text-3xl lg:text-5xl xl:text-7xl`}>
       {title}
     </h1>
   );
 }
-
--- src/components/common/text/paragraph.jsx
+++ src/components/common/text/paragraph.jsx
@@ -1,11 +1,7 @@
-export default function Description({
-  description,
-  className = "text-center",
-}) {
+export default function Paragraph({ description, className = "text-center" }) {
   return (
     <p className={`${className} text-xs lg:text-lg xl:text-2xl text-wrap`}>
       {description}
     </p>
   );
 }
-
