fb6f5ab9820da2d5677ff6c138c2a706b8258d69
Athul Anoop <athulanoop2020@gmail.com>
2025-01-26 12:33:08 +0530

    update: variants for headings

--- src/components/common/text/heading.jsx
+++ src/components/common/text/heading.jsx
@@ -1,7 +1,9 @@
-export default function Heading({ title, className = "" }) {
-  return (
-    <h1 className={`${className} text-xl sm:text-3xl lg:text-5xl xl:text-7xl`}>
-      {title}
-    </h1>
-  );
+const variants = {
+  lg: "text-xl sm:text-3xl lg:text-5xl xl:text-7xl",
+  xl: "text-2xl sm:text-3xl lg:text-5xl xl:text-7xl",
+  "2xl": "text-3xl sm:text-3xl lg:text-5xl xl:text-7xl",
+};
+
+export default function Heading({ title, className = "", variant = "lg" }) {
+  return <h1 className={`${className} ${variants[variant]}`}>{title}</h1>;
 }
