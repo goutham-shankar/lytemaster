1f01dd9ec2d0224290a369d59566ab16376d9e08
Athul Anoop <athulanoop2020@gmail.com>
2025-01-28 22:33:43 +0530

    update: replaced title and other text with custom text components with responsive behavior

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -7,13 +7,14 @@ import BentoGrid, { bento } from "@components/projects/bentoGrid";
 import Filters from "@components/projects/filters";
 import useProjectsStore from "@/store/projects";
 import { projectsData } from "@assets/placeholders";
+import { Heading, Paragraph } from "@components/common/text";
 
 const filters = ["Commercial", "Industrial", "Landscape"];
 
 const SectionTitle = ({ title }) => {
   return (
     <div className="w-full flex flex-col justify-center items-start gap-2">
-      <h1 className="w-min text-2xl text-left font-bold text-nowrap sm:text-4xl">
+      <h1 className="w-min text-xl text-left font-bold text-nowrap sm:text-2xl lg:text-3xl xl:text-4xl">
         {title}
         <span className="block w-1/3 h-0.5 mt-1 bg-black"></span>
       </h1>
@@ -23,9 +24,9 @@ const SectionTitle = ({ title }) => {
 
 const OverviewSection = ({ title, description }) => {
   return (
-    <section className="w-full h-max mt-12 px-8 py-8 flex flex-col justify-center items-center gap-4 text-black sm:px-16 sm:mt-16 md:mt-20 lg:mt-28">
-      <h1 className="w-full text-4xl text-left font-bold">{title}</h1>
-      <p className="">{description}</p>
+    <section className="w-full h-max mt-16 px-8 py-4 flex flex-col justify-center items-left gap-4 text-black sm:px-16 sm:py-8 sm:gap-6 md:mt-20 lg:gap-8 lg:mt-28 xl:gap-12">
+      <Heading title={title} />
+      <Paragraph description={description} className="text-left sm:w-1/2" />
       <Filters filters={filters} />
     </section>
   );
@@ -33,7 +34,7 @@ const OverviewSection = ({ title, description }) => {
 
 const ProjectSection = ({ title, cta, projects }) => {
   return (
-    <section className="w-full h-max px-8 py-8 flex flex-col justify-center items-center gap-6 text-black sm:px-16">
+    <section className="w-full h-max px-8 py-4 flex flex-col justify-center items-center gap-6 text-black sm:px-16 sm:py-8">
       <SectionTitle title={title} />
       <BentoGrid items={projects} />
       <CtaButton name={cta.name} href={cta.href} />
