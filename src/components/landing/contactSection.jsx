01759fa435013eba7bdccaefbb4d999e59549bdf
Athul Anoop <athulanoop2020@gmail.com>
2025-01-28 20:36:46 +0530

    update: button hover animations tweaked

--- src/components/landing/contactSection.jsx
+++ src/components/landing/contactSection.jsx
@@ -23,7 +23,7 @@ export default function ContactSection({ title, description, cta }) {
         </div>
         <Link
           href={cta.href}
-          className="px-8 py-2 text-white border border-white rounded-full hover:bg-white hover:text-black xl:text-2xl xl:px-12 xl:py-3"
+          className="px-8 py-2 text-white border border-white rounded-full hover:bg-white hover:text-black xl:text-2xl xl:px-12 xl:py-3 duration-500 transition ease-in-out hover:scale-105"
         >
           {cta.text}
         </Link>
