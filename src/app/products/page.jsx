0994516cebfe124217500edd37575cb317da2913
S SIDHARTH <sidharthpunalur@gmail.com>
2025-03-08 06:55:08 +0530

    /products page completed

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -2,7 +2,8 @@
 import CommercialImage from '@/assets/landing/commercial-lighting-thumbnail.jpeg'; // Adjust the path
 import IndustrialImage from '@/assets/landing/industrial-lighting-thumbnail.jpeg'; // Adjust the path
 import LandscapeImage from '@/assets/landing/hero-placeholder.jpeg'; // Adjust the path
-import { GiBulb } from "react-icons/gi";
+
+import Image from 'next/image';
 
 const products = [
   {
@@ -34,15 +35,14 @@ export default function ProductsPage() {
       </div>
 
       {/* Main Content */}
+      <h2 className="text-5xl font-bold lg:mx-32 mx-5">Our Products</h2>
       <main className="flex-1 max-w-6xl mx-auto px-4 py-12">
-        <h2 className="text-4xl font-bold mb-12">Our Products</h2>
-        
         <div className="grid md:grid-cols-3 gap-8 mb-24">
           {products.map((product) => (
             <a href="/products/category" key={product.title}>
             <div 
               key={product.title} 
-              className="h-auto rounded-2xl shadow-md overflow-hidden flex flex-col justify-between" 
+              className="h-[430px] rounded-2xl shadow-md overflow-hidden flex flex-col justify-between" 
               style={{ backgroundImage: `url(${product.image.src})`, backgroundSize: "cover", backgroundPosition: "center" }}
             >
               <div className="py-12 p-6 bg-gradient-to-b from-black via-transparent to-transparent">
@@ -51,9 +51,10 @@ export default function ProductsPage() {
               </div>
 
               {/* Moved this to the bottom */}
-              <div className="flex items-center justify-center gap-2 text-lg font-medium text-white bg-blur rounded-full mx-12 p-4 text-center shadow-md shadow-black backdrop-blur-md m-4">
-                <GiBulb className="text-white text-2xl" /> 
+              <div className=" flex items-center justify-evenly gap-2 text-lg font-medium text-white bg-blur rounded-full mx-12 p-4 text-center shadow-md shadow-black backdrop-blur-md m-4">
+              <Image src="/assets/light_bulb_products.png" width={24} height={24} alt="Bulb" />
                 {product.count} Products
+                <div></div>
               </div>
             </div>
             </a>
