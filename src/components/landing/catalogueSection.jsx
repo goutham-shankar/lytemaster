092d953572b43c50ad1a24c60aae2f27c68f6b9d
Athul Anoop <athulanoop2020@gmail.com>
2025-03-22 12:50:56 +0530

    update: corrected title

--- src/components/landing/catalogueSection.jsx
+++ src/components/landing/catalogueSection.jsx
@@ -6,7 +6,7 @@ export default function CatalogueSection() {
       id="catalogue"
       className="h-max p-8 flex flex-col justify-center items-start gap-6 text-black sm:px-16 sm:py-16 lg:px-20"
     >
-      <Heading title="Catalogue" variant="2xl" />
+      <Heading title="Catalogs" variant="2xl" />
       <div className="w-full flex flex-col justify-between items-center gap-5 sm:flex-row">
         {Array(5)
           .fill(0)
