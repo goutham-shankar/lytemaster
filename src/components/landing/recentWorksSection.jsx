9577a1b16ac4d760aa48ca5bc5a1ad96c4e4f4e1
Athul Anoop <athulanoop2020@gmail.com>
2025-03-22 13:24:28 +0530

    update: carousel responsiveness

--- src/components/landing/recentWorksSection.jsx
+++ src/components/landing/recentWorksSection.jsx
@@ -5,11 +5,9 @@ export default function RecentWorksSection({ title, projects }) {
   return (
     <section
       id="industries"
-      className="h-max py-8 flex flex-col justify-center items-start gap-6 text-black sm:px-16 sm:py-16 lg:px-20"
+      className="h-max p-8 flex flex-col justify-center items-start gap-6 text-black sm:px-16 sm:py-16 lg:px-20"
     >
-      <div className="px-8">
-        <Heading title={title} variant="2xl" />
-      </div>
+      <Heading title={title} variant="2xl" />
       <Carousel items={projects} />
     </section>
   );
