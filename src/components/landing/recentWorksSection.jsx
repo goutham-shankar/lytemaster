cf3f84aea060d68112d21e2591bcca7ebded34bf
Athul Anoop <athulanoop2020@gmail.com>
2025-03-22 13:09:13 +0530

    update: integrated carousel component

--- src/components/landing/recentWorksSection.jsx
+++ src/components/landing/recentWorksSection.jsx
@@ -1,13 +1,16 @@
 import { Heading } from "@components/common/text";
-import BentoGrid from "@components/projects/bentoGrid";
+import Carousel from "@components/common/carousel";
+
 export default function RecentWorksSection({ title, projects }) {
   return (
     <section
       id="industries"
-      className="h-max p-8 flex flex-col justify-center items-start gap-6 text-black sm:px-16 sm:py-16 lg:px-20"
+      className="h-max py-8 flex flex-col justify-center items-start gap-6 text-black sm:px-16 sm:py-16 lg:px-20"
     >
-      <Heading title={title} variant="2xl" />
-      <BentoGrid items={projects} />
+      <div className="px-8">
+        <Heading title={title} variant="2xl" />
+      </div>
+      <Carousel items={projects} />
     </section>
   );
 }
