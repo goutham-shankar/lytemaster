45e95865254e286eb44bdb67d13f3c4c3c9f9493
navadeep satheesh <obsidyne@gmail.com>
2025-03-22 15:43:01 +0530

    fdfd

--- src/components/landing/catalogueSection.jsx
+++ src/components/landing/catalogueSection.jsx
@@ -7,16 +7,16 @@ export default function CatalogueSection() {
       className="h-max p-8 flex flex-col justify-center items-start gap-6 text-black sm:px-16 sm:py-16 lg:px-20"
     >
       <Heading title="Catalogs" variant="2xl" />
-      <div className="w-full flex flex-col justify-between items-center gap-5 sm:flex-row">
+      <div className="w-full flex flex-row  justify-center items-center gap-[15px]  --bg-[red] items-center flex-wrap">
         {Array(5)
-          .fill(0)
+          .fill(0)  
           .map((_, i) => (
             <div
               key={i}
-              className="w-56 h-64 xl:w-64 xl:h-80 bg-black/80 rounded-lg"
+              className="w-[100px] h-[150px] xl:w-64 xl:h-80 bg-black/80 rounded-lg"
             ></div>
           ))}
       </div>
     </section>
-  );
+  );  
 }
