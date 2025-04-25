2d4abee33a361ae04d94481ed52871b095251807
Athul Anoop <athulanoop2020@gmail.com>
2025-04-25 18:16:00 +0530

    update: move button component for 'Load More' into bento grid component

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -12,47 +12,49 @@ import { Heading, Paragraph } from "@components/common/text";
 const filters = ["Commercial", "Industrial", "Landscape"];
 
 const SectionTitle = ({ title }) => {
-  return (
-    <div className="w-full flex flex-col justify-center items-start gap-2">
-      <h1 className="w-min text-xl text-left font-bold text-nowrap sm:text-2xl lg:text-3xl xl:text-4xl">
-        {title}
-        <span className="block w-1/3 h-0.5 mt-1 bg-black"></span>
-      </h1>
-    </div>
-  );
+    return (
+        <div className="w-full flex flex-col justify-center items-start gap-2">
+            <h1 className="w-min text-xl text-left font-bold text-nowrap sm:text-2xl lg:text-3xl xl:text-4xl">
+                {title}
+                <span className="block w-1/3 h-0.5 mt-1 bg-black"></span>
+            </h1>
+        </div>
+    );
 };
 
 const OverviewSection = ({ title, description }) => {
-  return (
-    <section className="w-full h-max mt-16 px-8 py-4 flex flex-col justify-center items-left gap-4 text-black sm:px-16 sm:py-8 sm:gap-6 md:mt-20 lg:gap-8 lg:mt-28 xl:gap-12">
-      <Heading title={title} />
-      <Paragraph description={description} className="text-left sm:w-1/2" />
-      <Filters filters={filters} />
-    </section>
-  );
+    return (
+        <section className="w-full h-max mt-16 px-8 py-4 flex flex-col justify-center items-left gap-4 text-black sm:px-16 sm:py-8 sm:gap-6 md:mt-20 lg:gap-8 lg:mt-28 xl:gap-12">
+            <Heading title={title} />
+            <Paragraph
+                description={description}
+                className="text-left sm:w-1/2"
+            />
+            <Filters filters={filters} />
+        </section>
+    );
 };
 
 const ProjectSection = ({ title, cta, projects }) => {
-  return (
-    <section className="w-full h-max px-8 py-4 flex flex-col justify-center items-center gap-6 text-black sm:px-16 sm:py-8">
-      <SectionTitle title={title} />
-      <BentoGrid items={projects} />
-      <CtaButton name={cta.name} href={cta.href} />
-    </section>
-  );
+    return (
+        <section className="w-full h-max px-8 py-4 flex flex-col justify-center items-center gap-6 text-black sm:px-16 sm:py-8">
+            <SectionTitle title={title} />
+            <BentoGrid items={projects} cta={cta} />
+        </section>
+    );
 };
 
 export default function Projects() {
-  const { filters } = useProjectsStore();
+    const { filters } = useProjectsStore();
 
-  return (
-    <>
-      <OverviewSection {...projectsData.overviewSection} />
-      {projectsData.projects
-        .filter((project) => filters.includes(project.tag))
-        .map((project, index) => (
-          <ProjectSection key={index} {...project} />
-        ))}
-    </>
-  );
+    return (
+        <>
+            <OverviewSection {...projectsData.overviewSection} />
+            {projectsData.projects
+                .filter((project) => filters.includes(project.tag))
+                .map((project, index) => (
+                    <ProjectSection key={index} {...project} />
+                ))}
+        </>
+    );
 }
