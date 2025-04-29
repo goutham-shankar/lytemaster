b81ffcd6c91f46ce05fcf724c36dc0cd552af3a4
obsidyne <obsidyne@gmail.com>
2025-04-29 23:31:44 +0530

    fdf

--- src/components/projects/filters.jsx
+++ src/components/projects/filters.jsx
@@ -33,7 +33,7 @@ export default function Filters({ filters }) {
   };
 
   return (
-    <div className="--px-4 mx-[auto] --mx-[45px] w-[94%] --px-4 py-2 md:p-6 --bg-red-200 w-full  mt-16    sm:px-16 sm:py-8 sm:gap-6 md:mt-20 lg:gap-8 lg:mt-28 xl:gap-12">
+    <div className="--px-4 mx-[auto] --mx-[45px] w-[90%] --px-4 py-2 md:p-6 --bg-red-200 w-full  mt-16    sm:px-16 sm:py-8 sm:gap-6 md:mt-20 lg:gap-8 lg:mt-28 xl:gap-12">
       {/* Desktop View - Always visible on larger screens */}
       <div className="hidden md:flex md:flex-col md:gap-4 md:w-full">
         <div className="w-full flex flex-row justify-between items-center">
